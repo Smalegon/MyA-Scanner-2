27efacbb99495fc48004506a0830b48790254297
Smalegon <dgonzalesmesco@gmail.com>
2026-09-02 18:08:19 -0500

    Update build.gradle.kts
    update a 2.3.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // Archivo de configuración raíz. No necesitas tocar nada aquí.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-}
+    id("com.android.application") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
